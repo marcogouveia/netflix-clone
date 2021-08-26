4bc8eea400a8c280488b096e086e10a130bda204
Marco Gouveia <marcogouveia2001@outlook.com>
2021-08-25 21:51:23 -0300

    90% da Primeira página

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var acc = document.getElementsByClassName("accordion-button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+
+        console.log(panel);
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
